45716a1d6315999d3aee54759234ea80cd5b9957
Gleb932 <gleb.g932@gmail.com>
2025-04-07 16:46:08 +0200

    Small fix for album and artist cards

--- app/src/main/java/com/example/musicplayer/ui/ArtistCard.kt
+++ app/src/main/java/com/example/musicplayer/ui/ArtistCard.kt
@@ -4,9 +4,9 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MusicNote
@@ -50,8 +50,8 @@ fun ArtistCard(
                 fallback = notePainter,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .width(128.dp)
                     .height(128.dp)
+                    .fillMaxWidth()
                     .align(Alignment.CenterHorizontally)
                     .background(Color.White)
             )
